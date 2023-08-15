e9c344dfc3042119f8805e1525f87a6ea9c65f6c
Darin Milner <darinmcodingprojects@gmail.com>
2023-08-15 19:10:51 +0700

    Internet Gateway code added

--- vpc.py
+++ vpc.py
@@ -32,3 +32,33 @@ else:
         "Value" : vpc_name
     }]
     print(f"VPC {vpc_name} with ID {vpc_id} has been created")
+
+    # internet gateway
+    ig_name = "main-igw"
+
+
+#check if internet gateway exists
+response = ec2.describe_internet_gateways(
+    Filters=[
+        {"Name": "tag:Name", "Values": [ig_name]}
+    ]
+)
+
+internet_gateways = response.get("internetGateways", [])
+
+if internet_gateways:
+    # skip creation
+    ig_id = internet_gateways[0]["InternetGatewayId"]
+    print(f"Internet gateway with id {ig_id} already exists.")
+else:
+    ig_response = ec2.create_internet_gateway()
+
+    ig_id = ig_response["InternetGateway"]["InternetGatewayId"]
+
+    ec2.create_tags[ig_id], Tags=[{
+        "Key": "Name",
+        "Value" : ig_name
+    }]
+    # attach to VPC
+    ec2.attach_internet_gateway(VpcId=vpc_id, InternetGatewayId=ig_id)
+    print(f"Internet Gateway {ig_name} with ID {ig_id} has been created")
